dcf6fe34ebd7036cede1599c889ea2f7ee4bf1b0
Yuk Kei Wan <41866052+yukkeiwan96@users.noreply.github.com>
2019-02-26 09:50:08 +0800

    Add files via upload

---
+++ unsupervised.py
@@ -0,0 +1,153 @@
+import sys
+import random
+
+#spambase has 58 columns <- no missing attributes
+#iris has 5 columns <- no missing attributes
+#glass has 11 columns <- no missing attributes
+
+rawdata=sys.argv[1]  #opens the dataset
+numcol=int(sys.argv[2])   #input the number of columns in the dataset
+
+def openFile(rawdata,numcol):
+ file=open(rawdata,"r")
+ stored={}
+ for ln in file:
+  ln=ln.strip("\r\n")
+  ln=ln.split(",")
+  for n in range(0,numcol):
+   if n not in stored:
+    stored[n]=[ln[n]]
+   else:
+    stored[n].append(ln[n])
+ return stored
+
+dat=openFile(rawdata,numcol)
+
+#the following three functions are to remove the columns that are
+#the sample number and the associated class from the data.
+def preGlass(dat):
+ del dat[0]  #removes sample number
+ del dat[10] #removes associated class
+ return dat
+
+def preIris(dat):
+ del dat[4] #removes associated class
+ return dat
+
+def preSpam(dat):
+ del dat[57] #removes associated class
+ return dat
+
+Dlist= preSpam(dat)
+#Dlist= preIris(dat,F)[0]
+#Dlist= preGlass(dat,F)[0]
+
+def featureSet(Dlist):
+ F=[]
+ for f in Dlist:
+  F.append(f)
+ return F
+
+def kmeans(D,k):
+ m=random.sample(D,k)
+ m=list(map(float,m))
+ clusters=[]
+ for n in range(0,k):
+  v=[m[n]]
+  clusters.append(v)
+ for x in D:
+  nc=[]
+  for n in range(0,k):
+   dist=abs(float(x)-m[n])
+   nc.append(dist)
+  locx=nc.index(min(nc))
+  clusters[locx].append(float(x))
+ kmeans=[]
+ for v in clusters:
+  new=sum(v)/len(v)
+  kmeans.append(new)
+ return (kmeans,clusters)
+
+runKmeans=kmeans(Dlist[55],2)
+centrioles=runKmeans[0]
+clusters=runKmeans[1]
+
+def Silhouette(clusters):
+ svalues=[]
+ numCluster=len(clusters)
+ curloc=0
+ otherIndex=[]              
+ for cluster in clusters:      #1st loop: loop through each of the cluster in the list of all clusters
+  for i in range(0,numCluster):      #find the cluster(s) that's not the current one
+   if i != curloc:
+    otherIndex.append(i)
+  others = [clusters[i] for i in otherIndex] #create a list of the clusters' indices
+#  for o in others:    
+#   print(o[0:5])
+#  print(cluster[0:5])
+  curloc+=1             #move the location to the next cluster in the 1st loop
+  otherIndex=[]         #default the index list for other cluster(s)
+  inCluster=[]          #initiates a list storing all the average distance inside a cluster
+  outCluster=[]         #initiates a list storing all the average distance outside a cluster
+  loc=0                 #a counter that specify the location of a point with in a cluster
+  for p in cluster:     #2nd loop: looping through all the points in a cluster
+   ptot=[]              #initializes a list for point-to-point distances
+   otot=[]
+   temp=cluster         #set a temporary list of all the points in the cluster,
+   del temp[loc]        # and delete the current point
+   for x in temp:       #3rd loop: compares the current point to all other points
+    dist=abs(p-x)       #get the absvalue of the distance between the current point with others
+    ptot.append(dist)   #append to the point-to-point distance list
+   pavg=sum(ptot)/len(ptot)  #get the average distance from the list
+   inCluster.append(pavg)    #append the average of that point to the list storing average distance
+   otherL=[]            
+   for other in others: #loops through all the other cluster(s)
+    for y in other:     #loops through all the points in a cluster
+     dist=abs(p-y)      #get the absvalue of the distance between the current point with a point from another cluster
+     otot.append(dist)  #append to the point-to-point distance list 
+    oavg=sum(otot)/len(otot)  #get the average distance from the list 
+    outCluster.append(oavg)   #append the average of that point to the list storing average distance
+    otherL.append(outCluster) #appending a list of point-to-point distances between the current cluster to the current other cluster
+   loc+=1               #moving to the point in the next position
+  outC=[]
+  if len(otherL)>1:
+   en=len(otherL[1])
+   for pa in range(0,en-1):
+    if otherL[0][pa]>=otherL[1][pa]:
+     outC.append(otherL[1][pa])
+    else:
+     outC.append(otherL[0][pa])
+  elif len(otherL)==1:
+   outC=otherL[0]
+  allpts=len(outC)
+  for i in range(0,allpts-1):
+   if outC[i] >= inCluster[i]:
+    maxi= outC[i]
+   elif outC[i] < inCluster[i]:
+    maxi= inCluster[i]
+   s=((outC[i]-inCluster[i])/maxi)
+   svalues.append(s)
+ scoef=sum(svalues)/len(svalues)
+ return scoef
+
+#print(Silhouette(clusters)) 
+
+def SFS(F,D):
+ Fzero=[]
+ basePerf=-100000
+ bestPerf=-100000
+ while len(F) > 0:
+  for f in F:
+   Fzero.append(f)
+   h=kmeans(Fzero[-1],D)
+   currentPerf=Perf(h,D)
+   if currentPerf > bestPerf:
+    bestPerf= currentPerf
+    bestF= f
+  if bestPerf>basePerf:
+   basePerf=bestPerf
+   F.remove(bestF)
+   Fzero.append(bestF)
+ return Fzero
+
+
