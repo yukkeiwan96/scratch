a9d7c7c9ea72a88c9da1b2f6f1c77383d7c19cca
Yuk Kei Wan <41866052+yuukiiwa@users.noreply.github.com>
2020-02-13 07:09:54 +0800

    Add files via upload

---
+++ generateFigures.R
@@ -0,0 +1,51 @@
+counts=read.table(file="DNMT1-PARP1.csv",sep="\t")
+colnames(counts)<-c("combination","G","FC")
+df<-counts
+df$G<- as.factor(df$G)
+head(df)
+log2FC<-df$FC
+Combinations<-df$G
+Type<-df$combination
+library(ggplot2)
+ggplot(df, aes(x=Combinations, y=log2FC, color=Type)) +
+  geom_point(size=4, alpha=0.7, position=position_jitter(w=0.1, h=0)) +
+  stat_summary(fun.y=mean, geom="point", shape=23, color="black", size=4) +         
+  stat_summary(fun.ymin=function(x)(mean(x)-sd(x)), 
+               fun.ymax=function(x)(mean(x)+sd(x)),
+               geom="errorbar", width=0.1) +
+  theme_bw()
+
+counts=read.table(file="Nick_pairwise_CV1_sgRNA.csv",sep=",")
+colnames(counts)<-c("combination","FC")
+df<-counts
+head(df)
+log2FC<-df$FC
+Combinations<-df$combination
+ggplot(df, aes(x=df$combination, y=df$FC)) + 
+  geom_point(col="tomato2", size=3) 
+
+counts=read.table(file="Book1.csv",sep=",")
+colnames(counts)<-c("model","type","MSE")
+df<-counts
+df$model<- as.factor(df$model)
+head(df)
+Model<-df$model
+MSE<-df$MSE
+Type<-df$type
+library(ggplot2)
+ggplot(df, aes(x=model, y=MSE, color=Type)) +
+  geom_point(size=4, alpha=0.7, position=position_jitter(w=0.1, h=0)) +
+  stat_summary(fun.y=mean, geom="point", shape=23, color="black", size=4) +         
+  stat_summary(fun.ymin=function(x)(mean(x)-sd(x)), 
+               fun.ymax=function(x)(mean(x)+sd(x)),
+               geom="errorbar", width=0.1) +
+  theme_bw()
+
+library("devtools")
+install_github("omarwagih/ggseqlogo")
+require(ggplot2)
+require(ggseqlogo)
+tab=read.table(file="bottom100put_random_results.csv",sep=",")
+dnalist=as.vector(tab$V3)
+ggseqlogo(dnalist)
+ggplot() + geom_logo(dnalist, col_scheme='base_pairing') + theme_logo()
