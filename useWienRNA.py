b345384fdad1f161bb391cca7f5883e92fdc3d16
Yuk Kei Wan <41866052+yuukiiwa@users.noreply.github.com>
2020-02-12 10:49:27 +0800

    Update useWienRNA.py

--- useWienRNA.py
+++ useWienRNA.py
@@ -15,5 +15,86 @@ sequence = "CGCAGGGAUACCCGCG"
 fc = RNA.fold_compound(sequence)
 # compute minimum free energy (mfe) and corresponding structure
 (ss, mfe) = fc.mfe()
-# print output
-print "%s [ %6.2f ]" % (ss, mfe)
+
+def compare(mint,nint):
+ if nint > mint:
+  mint=nint
+ return mint
+
+def SStruct(ln):
+ S1=ln.split("(.")
+ S=[]
+ for sub in S1:
+  if "(" in sub and ")" in sub:
+   subl=sub.split(").")
+   if len(subl) > 1:
+    for s in subl:
+     if ")" in s:
+      s+=")."
+     else:
+      s+="(."
+     S.append(s)
+   else:
+    subl=sub.split(")(")
+    for s in subl:
+     if ")" in s:
+      s+=")"
+     else:
+      s="("+s+"(."
+     S.append(s)
+  else:
+   if ")" not in sub:
+    sub+="(."
+    S.append(sub)
+   else:
+    subl=sub.split(").")
+    for s in subl[:-1]:
+     s+=")."
+     S.append(s)
+    S.append(subl[-1])
+ print(ln)
+ a=""
+ for i in S:
+  a+=i
+ print(a)
+ print(S)
+ MCstem=0
+ for j in range(len(S)):
+  cstem=S[j].count("(")+S[j].count(")")
+  MCstem=compare(MCstem,cstem)
+ loops,Mloop,Mstem,snt0,snt1=[],0,0,0,0
+ for j in range(len(S)):
+  if j == 0:
+   loop=[S[j]]
+  else:
+   if "(" in S[j] and "(" in loop[-1]:
+    loop.append(S[j])
+   elif "(" in loop[-1] and ")" in S[j]:
+    loop.append(S[j])
+   elif ")" in S[j] and ")" in loop[-1]:
+    loop.append(S[j])
+   elif ")" in loop[-1] and "(" in S[j]:
+    loops.append(loop)
+    loop=[S[j]]
+ loops.append(loop)
+ stems1,stems2="",""
+ for loop in loops:
+  stem1,stem2="",""
+  for a in range (len(loop)):
+   if a+1 < len(loop):
+    if loop[a].endswith("(.") and loop[a+1].startswith("."):
+     cloop=len(loop[a])-loop[a].find("(.")-1+loop[a+1].find(")")
+     Mloop=compare(Mloop,cloop)
+   if "(" in loop[a]:
+    stem1+=loop[a]
+   elif ")" in loop[a]:
+    stem2+=loop[a]
+  stems=[stem1,stem2]
+  stems1+=stem1
+  stems2+=stem2
+  for stem in stems:
+   snt=stem.count("(")+stem.count(")")
+   Mstem=compare(Mstem,snt)
+ allbp=compare(stems1.count("("),stems2.count(")"))
+ print(Mloop,MCstem,Mstem,allbp)
+SStruct(ss)
