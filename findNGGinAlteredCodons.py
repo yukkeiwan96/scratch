d109250b6df114567d903a087f5148c3cc359886
Yuk Kei Wan <41866052+yuukiiwa@users.noreply.github.com>
2020-09-11 16:04:31 +0800

    Update findNGGinAlteredCodons.py

--- findNGGinAlteredCodons.py
+++ findNGGinAlteredCodons.py
@@ -1,4 +1,4 @@
-import sys
+import sys, random
 fn,pattern=sys.argv[1],sys.argv[2]
 gencode = {
     'ATA':'I', 'ATC':'I', 'ATT':'I', 'ATG':'M',
@@ -18,13 +18,39 @@ gencode = {
     'TAC':'Y', 'TAT':'Y', 'TAA':'_', 'TAG':'_',
     'TGC':'C', 'TGT':'C', 'TGA':'_', 'TGG':'W'}
 
-
+aaDict={}
+for codon in gencode:
+ aa=gencode[codon]
+ if aa not in aaDict:
+  aaDict[aa]=[codon]
+ else:
+  aaDict[aa].append(codon)
 
 file=open(fn,"r")
 for CDS in file:
- CDS=upper(CDS.strip())
+ CDS=CDS.strip().upper()
+ print(CDS)
  codonList,i=[],0
- for j in range(len(CDS)):
+ for j in range(1,len(CDS)):
   if j % 3 == 0:
    codonList.append(CDS[i:j])
-  i=j
+   i=j
+ codonList.append(CDS[i:])
+ aminoacid,aaList="",[]
+ for codon in codonList:
+  aminoacid+=gencode[codon]
+ for aa in aminoacid:
+  aaList.append(aaDict[aa])
+ mutList=[]
+ for i in range(50000):
+  newaaseq=""
+  for codons in aaList:
+   newaaseq+=random.choice(codons)
+  if newaaseq not in mutList:
+   pattern_occurance=newaaseq.count(pattern)
+   mutList.append((newaaseq,pattern_occurance))
+ mutList.sort(key=lambda tup: tup[1], reverse=True)
+ outfile=open(aminoacid+"_"+pattern+".csv","w")
+ for tup in mutList:
+  outfile.write(tup[0]+","+str(tup[1])+"\r\n")
+ outfile.close()
